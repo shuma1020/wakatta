289cc023cf08aa5dc3ac32e7863a437ed687c3a7
shuma1020 <shumaseki40@gmail.com>
2019-12-12 10:21:33 +0900

    adding layout member model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+    def page_title
+        title = "Wakatta"
+        title = @page_title + "-" + title if @page_title
+        title
+    end
+    def menu_link_to(text, path, options ={})
+        content_tag :li do
+            link_to_unless_current(text, path, options) do
+                content_tag(:span, text)
+            end
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
   root to: "posts#index"
+  get "/about", to: "posts#about"
 end
