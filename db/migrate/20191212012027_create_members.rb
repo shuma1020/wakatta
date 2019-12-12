bc40fdc641ee95bb3457bbf60ea7ef50a748d19a
shuma1020 <shumaseki40@gmail.com>
2019-12-12 10:26:47 +0900

    adding specific info to member model

--- db/migrate/20191212012027_create_members.rb
+++ db/migrate/20191212012027_create_members.rb
@@ -1,7 +1,11 @@
 class CreateMembers < ActiveRecord::Migration[6.0]
   def change
     create_table :members do |t|
-
+      t.string :name, null: false
+      t.string :full_name
+      t.string :email
+      t.date :birthday
+      t.boolean :administration, null: false, default: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_11_121627) do
+ActiveRecord::Schema.define(version: 2019_12_12_012027) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "members", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "full_name"
+    t.string "email"
+    t.date "birthday"
+    t.boolean "administration", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
