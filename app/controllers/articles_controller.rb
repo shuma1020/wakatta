cee706ce89a47c7b8638adcd25c820eda7345b68
shuma1020 <shumaseki40@gmail.com>
2019-12-15 14:07:29 +0900

    finish 9.1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,43 @@ class ArticlesController < ApplicationController
     before_action :login_required, except: [:index, :show]
 
     def index
-        @articles = Article.order(released_at :desc)
+        @articles = Article.order(released_at: :desc)
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(params[:article])
+        if @article.save
+            redirect_to @article, notice: "ニュースを登録しました"
+        else
+         render "new"
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        @article.assign_attributes(params[:article])
+        if @article.save
+            redirect_to @article, notice: "ニュースを更新しました。"
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to :articles
     end
 end
