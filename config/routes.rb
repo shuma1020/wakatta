95ee4f68f1da79494ca8a6525cfff8936d23be23
shuma1020 <shumaseki40@gmail.com>
2019-12-17 10:19:57 +0900

    finish make entry controller p376

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'entries/index'
+  get 'entries/show'
+  get 'entries/new'
+  get 'entries/edit'
   get 'passwords/edit'
   get 'accounts/show'
   get 'accounts/edit'
   get 'top/index'
   resources :members do
     get "search", on: :collection
+    resources :entries, only: [:index]
   end
   root  "top#index"
   get "about", to: "top#about", as: "about"
