c67cbd56d5b3c34f23181e6936da5132ea5a381e
shuma1020 <shumaseki40@gmail.com>
2019-12-17 18:06:49 +0900

    finish p380

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,25 +1,26 @@
 class Entry < ApplicationRecord
     belongs_to :author, class_name: "Member", foreign_key: "member_id"
-
-    STATUS_VALUES = %(draft member_only public)
-
-    validates :title, presence: true, length: {maximum: 200}
+  
+    STATUS_VALUES = %w(draft member_only public)
+  
+    validates :title, presence: true, length: { maximum: 200 }
     validates :body, :posted_at, presence: true
-    validates :stauts, inclusion: {in: STATUS_VALUES}
-
-    scope :common, ->  {where(status: "public")}
-    scope :published, -> {where("status <> ?", "draft")}
-    scope :full, -> (member){
-        where("status <> ? OR member_id = ?", "draft", member.id)}
-    scope :readable_for, ->(member){ member ? full(member) :common}
-
+    validates :status, inclusion: { in: STATUS_VALUES }
+  
+    scope :common, -> { where(status: "public") }
+    scope :published, -> { where("status <> ?", "draft") }
+    scope :full, ->(member) {
+      where("member_id = ? OR status <> ?", member.id, "draft") }
+    scope :readable_for, ->(member) { member ? full(member) : common }
+  
     class << self
-        def status_text(status)
-            I18n.t("activerecord.attributes.entry.status_#{status}")
-        end
-
-        def status_options
-            STATUS_VALUES.map {|status| [status_text(status), status]}
-        end
+      def status_text(status)
+        I18n.t("activerecord.attributes.entry.status_#{status}")
+      end
+  
+      def status_options
+        STATUS_VALUES.map { |status| [status_text(status), status] }
+      end
     end
-end
+  end
+  
