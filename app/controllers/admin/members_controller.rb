bd1c1f7faa9f2cac09a5e790be3611c054e08760
shuma1020 <shumaseki40@gmail.com>
2019-12-18 15:56:43 +0900

    finish 520

---
+++ app/controllers/admin/members_controller.rb
@@ -0,0 +1,65 @@
+class Admin::MembersController < Admin::Base
+
+    def index
+        @members = Member.order("id")
+        .page(params[:page]).per(15)
+    end
+
+    def show
+        @member = Member.find(params[:id])
+    end
+
+    def new
+        @member = Member.new(birthday: Date.new(1980,1,1))
+    end
+
+    def edit
+        @member = Member.find(params[:id])
+    end
+
+    def create
+        @member = Member.new(member_params)
+        if @member.save
+            redirect_to [:admin, @member], notice: "会員を登録しました。"
+        else 
+            render "new"
+        end
+    end
+
+    def update
+        @member = Member.find(params[:id])
+        @member.assign_attributes(member_params)
+        if @member.save
+            redirect_to [:admin, @member], notice: "会員情報を更新しました。"
+        else
+            render "edit"
+        end
+    end
+
+    def destroy
+        @member = Member.find(params[:id])
+        @member.destroy
+        redirect_to :admin_members, notice: "会員を削除しました。"
+    end
+
+    def search
+        @members = Member.search(params[:q])
+            .page(params[:page]).per(15)
+        render "index"
+    end
+
+    private def member_params
+        attrs = [
+            :new_profile_picture,
+            :name,
+            :full_name,
+            :birthday,
+            :email,
+            :administrator
+        ]
+
+        attr << :password if params[:action] == "create"
+
+        params.require(:member).permit(attrs)
+    end
+end
